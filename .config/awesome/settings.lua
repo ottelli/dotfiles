ec64541d314078cd04e3e046f15ddb8fd5f89ea4
ottelli <business.ottelli@gmail.com>
2023-04-15 13:06:46 +0100

    add README.md

--- .config/awesome/settings.lua
+++ .config/awesome/settings.lua
@@ -1,16 +1,15 @@
 
 
 local _S = {
-    ['theme']           = "monad_clone",
-    ['colour_scheme']   = "mariner_blue", -- file in colourschemes
+    ['theme']           = "monad_clone", -- filename in custom_themes
+    ['colour_scheme']   = "mariner_blue", -- filename in colourschemes
     ['taskbar_style']   = "full", -- "icons", "full"
     ['show_titlebars']  = false,
-    -- ['tag_names']       = { "main", "www", "cod3", "4pi", "note5", "$6", "&7" },
     ['tag_names']       = { "M", "W", "C", "T", "5", "6", "7" },
     -- ['tag_names']       = { " 1 ", " 2 ", " 3 ", " 4 ", " 5 ", " 6 ", " 7 " },
     -- ['tag_names']       = { "1", "2", "3", "4", "5", "6", "7" },
     ['modkey']          = "Mod4",
-    ['terminal']        = "x-terminal-emulator", -- hand off to the system set list, $ 'sudo update-alternatives --config x-terminal-emulator' to edit
+    ['terminal']        = "x-terminal-emulator", -- GNOME : hand off to the system set list, $ 'sudo update-alternatives --config x-terminal-emulator' to edit
     ['editor']          = os.getenv("EDITOR") or "vim",
 }
 
---
+++ README.md
@@ -0,0 +1,20 @@
+# dotfiles
+
+Minimal and sensible configs I use on my two machines.
+
+My main setup that I use for all my software development work is Ubuntu with AwesomeWM.
+I have made a couple of themes for awesome and I like to tweak colour schemes so I tried to make this as easy as possible (just checkout .confg/awesome/settings.lua). Both take advantage of all the usual awesomewm stuff like workspace peeking, with intuitive keymaps for basic functions.
+
+![Powerline Theme](/dotfiles/assets/powerline-theme.png)
+![Monad Clone Theme](/dotifles/assets/monadclone-theme.png)
+
+I'm also keeping an old laptop alive with Arch + XMonad that I use to play around with Linux and do generally tinkering. (Hence the 'Monad Clone' theme :sunglasses:)
+
+Starship styles the terminal prompt in both setups, again I don't go crazy with it! Just minimal & sensible :ok_hand:
+![Starship Prompt](/dotfiles/assets/starship.png)
+
+## Screenshots
+
+![Main setup](/dotfiles/assets/screenshot.png)
+
+![with some windows open (with cpu usage tray open)](/dotfiles/assets/top.png)
